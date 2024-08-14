b4853641ee53fda36fd374b1e0f45c57083fb432
andrewpjlewis <lewiand1234@gmail.com>
2024-08-14 16:32:08 -0700

    etch a sketch done

--- etch-a-sketch/scripts/scripts.js
+++ etch-a-sketch/scripts/scripts.js
@@ -1,1 +1,22 @@
-console.log('linked');
+const container = document.getElementById('container');
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    container.innerHTML = '';
+    let sizeInput = prompt("Enter pixel count less than 100: ");
+    for (let i = 0; i < sizeInput ** 2; i++) {
+        container.innerHTML += '<div class="gridItem" style="width: 100%; height: 25px;"></div>';
+    }
+    container.style.display = 'grid';
+    container.style.width = '100%';
+    container.style.marginTop = '2em';
+    container.style.gridTemplateColumns = `repeat(${sizeInput}, 1fr)`;
+
+    const gridItem = document.querySelectorAll('.gridItem');
+    gridItem.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'lightgrey';
+        });
+    })
+});
+
